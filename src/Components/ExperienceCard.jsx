0a98707e2e140997374cb96554c0493a7c758fea
Darshan Pandya <andyadarshan811@gmail.com>
2024-06-04 23:25:18 +0530

    [Refactor] Styles in footer and changes in margins.

--- src/Components/ExperienceCard.jsx
+++ src/Components/ExperienceCard.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import { FiGithub } from "react-icons/fi";
-
+// border-2 border-white border-solid
 const ExperienceCard = ({experience}) => {
 
     const {ProjectName , Duration , Description , Github } = experience;
 
   return (
-    <div className="card my-8 experience-card cursor-pointer hover:border-[#6849f3] relative m-4  md:m-12 p-8 bg-black border-2 border-white border-solid md:w-[35rem] rounded-lg shadow-lg">
-        <h1 className="text-white text-2xl font-bold mb-2 tracking-wider">{ProjectName}</h1>
-        <h2 className="text-gray-300 text-base font-semibold mb-4">{Duration}</h2>
-        <p className="text-gray-400 leading-7">{Description}</p>
+    <div className="my-8 experience-card text-gray-300 hover:text-white hover:scale-105 transition-all duration-200 cursor-pointer bg-[#101010] relative m-4  md:m-12 p-8 md:w-[35rem] rounded-lg">
+        <h1 className="text-2xl font-bold mb-2 tracking-wider">{ProjectName}</h1>
+        <h2 className="text-base font-semibold mb-4">{Duration}</h2>
+        <p className=" leading-7">{Description}</p>
         <a target='_blank' href={Github} className='absolute top-4 right-4 hover:text-[#6849f3]'><FiGithub size={30}/></a>
     </div>
   )
