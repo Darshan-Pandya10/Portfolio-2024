0a024cc93c59dd73f8253cf6f1b2bacb0cf06f51
Darshan Pandya <andyadarshan811@gmail.com>
2024-06-06 18:01:12 +0530

    [Refactor] Navbar For Small Screen.

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -4,9 +4,9 @@ import { FaCode } from "react-icons/fa6";
 
 const Header = () => {
   return (
-    <section id='header' className='header pt-4 relative px-4 md:px-6 z-50 w-screen flex flex-col justify-around items-start sm:flex sm:flex-row sm:justify-between sm:items-center p-2'>
+    <section id='header' className='header fixed top-0 z-50 bg-black pt-4 px-4 md:px-6 w-screen flex flex-col justify-around items-start sm:flex sm:flex-row sm:justify-between sm:items-center p-2'>
       <main className="logo mb-1">
-        <a href=""><FaCode size={30} className='cursor-pointer -mt-1' /></a>
+        <a href=""><FaCode size={30} className='cursor-pointer' /></a>
       </main>
       <Navbar/>
     </section>
