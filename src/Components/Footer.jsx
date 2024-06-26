9980f05b165629c6e594b4f912b57e2b91edc2c3
Darshan Pandya <andyadarshan811@gmail.com>
2024-06-26 22:17:30 +0530

    [Refactor] Footer Links

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -11,8 +11,8 @@ const Footer = () => {
         <FaCode size={30} className='cursor-pointer' />
       </main>
       <main className="socials inline-block">
-          <a href='www.linkedin.com/in/darshanpandya811' className='bg-[#151515] hover:scale-110 duration-50 hover:bg-[#000000] p-3 rounded-full inline-block m-2 text-white '><GrLinkedinOption size={28}/></a>
-          <a href="https://github.com/Darshan-Pandya10" className='bg-[#151515] hover:scale-110 duration-50  hover:bg-[#000000]  p-3 rounded-full inline-block m-2 text-white '><FaGithub size={28}/></a>
+          <a href='www.linkedin.com/in/darshanpandya811' target='_blank' className='bg-[#151515] hover:scale-110 duration-50 hover:bg-[#000000] p-3 rounded-full inline-block m-2 text-white '><GrLinkedinOption size={28}/></a>
+          <a href="https://github.com/Darshan-Pandya10" target='_blank' className='bg-[#151515] hover:scale-110 duration-50  hover:bg-[#000000]  p-3 rounded-full inline-block m-2 text-white '><FaGithub size={28}/></a>
       </main>
     </section>
   )
