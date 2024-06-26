765907731128a6f63e7a7c540020a6311459a16a
Darshan Pandya <andyadarshan811@gmail.com>
2024-06-26 22:33:53 +0530

    [Changes] IN Files.

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -6,6 +6,7 @@ import { HiOutlineHome } from "react-icons/hi";
 import { FiSearch } from "react-icons/fi";
 import { MdOutlineBookmarkAdd } from "react-icons/md";
 import { CgDetailsMore } from "react-icons/cg";
+import {Resume} from '../assets/File/Resume.pdf'
 
 const Navbar = () => {
 
@@ -29,7 +30,7 @@ const Navbar = () => {
           <nav className={`mt-3 sm:mt-0 ${
           isNavbarVisible ? '' : 'hidden'} flex flex-col items-left justify-center justify-self-center place-items-center
           w-[90vw] mx-auto min-h-screen sm:flex sm:flex-row sm:justify-end sm:items-end  sm:min-h-fit sm:ml-4 font-semibold text-[1rem]`}>
-          <a href='../src/assets/File/Resume.pdf' download='Resume' onClick={closeNavbar} className='text-2xl tracking-widest sm:tracking-normal sm:text-base mb-1 sm:mr-3 sm:mb-0' to='/'>Resume</a>
+          <a href={Resume} download='Resume' onClick={closeNavbar} className='text-2xl tracking-widest sm:tracking-normal sm:text-base mb-1 sm:mr-3 sm:mb-0' to='/'>Resume</a>
           <a href='#projects' onClick={closeNavbar} className='text-2xl tracking-widest sm:tracking-normal sm:text-base  mb-1 sm:mr-3 sm:mb-0' to='about'>Projects</a>
           <a href='#contact' onClick={closeNavbar} className='text-2xl tracking-widest sm:tracking-normal sm:text-base  mb-1 sm:mr-3 sm:mb-0' to='about'>Contact</a>
         </nav>
